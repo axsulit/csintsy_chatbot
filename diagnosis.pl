9d99e1635dbe0c480fc670fae728a756a5fbda95
ciaramicaelag <125450878+ciaramicaelag@users.noreply.github.com>
2023-03-28 22:15:26 +0800

    Update diagnosis.pl

--- diagnosis.pl
+++ diagnosis.pl
@@ -19,6 +19,33 @@ disease(Patient, typhoid_fever) :-
     yes(upset_stomach, Patient),
     yes(diarrhea, Patient).
 
+disease(Patient, hepatitis_a) :-
+    yes(yellowing_of_skin_and_eyes, Patient),
+    yes(loss_of_appetite, Patient),
+    yes(upset_stomach, Patient),
+    yes(vomiting, Patient),
+    yes(stomach_pain, Patient),
+    yes(fever, Patient),
+    yes(joint_pain, Patient),
+    yes(fatigue, Patient).
+
+disease(Patient, tuberculosis) :-
+    yes(cough, Patient),
+    yes(pain_with_breathing_or_coughing, Patient),
+    yes(fever, Patient),
+    yes(chills, Patient),
+    yes(night_sweats, Patient),
+    yes(weight_loss, Patient),
+    yes(chest_pain, Patient),
+    yes(fatigue, Patient),
+    yes(weakness, Patient),
+    yes(slugishness, Patient),
+    yes(confusion, Patient),
+    yes(vomiting, Patient),
+    yes(bulging_soft_spot_on_the_head, Patient),
+    yes(poor_reflexes, Patient),
+    yes(loss_of_appetite, Patient).
+    
 disease(Patient, influenza) :-
     yes(fever, Patient),
     yes(cough, Patient),
