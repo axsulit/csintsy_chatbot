6346b78e3c15211c86abb6966b2be15b179f821e
zhoexaeris <zhoe_gongon@dlsu.edu.ph>
2023-04-04 00:20:00 +0800

    added all diseases

--- diagnosis.pl
+++ diagnosis.pl
@@ -6,6 +6,15 @@ weight(3, _, 0.8).
 weight(4, _, 1).
 
 % Define the symptoms and causes for each disease
+symptom_of(typhoid_fever, fever).
+symptom_of(typhoid_fever, headache).
+symptom_of(typhoid_fever, fatigue).
+symptom_of(typhoid_fever, cough).
+symptom_of(typhoid_fever, constipation).
+symptom_of(typhoid_fever, body_aches).
+cause_of(hepatitis_a, contaminated_food_or_beverages).
+cause_of(hepatitis_a, travel_to_tropical_regions).
+
 symptom_of(hepatitis_a, jaundice).
 symptom_of(hepatitis_a, lack_of_appetite).
 symptom_of(hepatitis_a, upset_stomach).
@@ -22,27 +31,21 @@ cause_of(hepatitis_a, sexually_active).
 cause_of(hepatitis_a, contact_with_bodily_fluids).
 cause_of(hepatitis_a, contaminated_food_or_beverages).
 
-symptom_of(influenza, fever).
-symptom_of(influenza, cough).
-symptom_of(influenza, sore_throat).
-symptom_of(influenza, runny_nose).
-symptom_of(influenza, body_aches).
-symptom_of(influenza, headache).
-symptom_of(influenza, fatigue).
-symptom_of(influenza, vomiting).
-symptom_of(influenza, diarrhea).
-cause_of(influenza, exposure_to_people_with_flu).
-
-symptom_of(schistosomiasis, rash).
-symptom_of(schistosomiasis, itchy_skin).
-symptom_of(schistosomiasis, muscle_pain).
-symptom_of(schistosomiasis, stomach_pain).
-symptom_of(schistosomiasis, blood_in_urine_and_feces).
-symptom_of(schistosomiasis, difficulty_urinating).
-symptom_of(schistosomiasis, enlarged_liver).
-symptom_of(schistosomiasis, cough).
-symptom_of(schistosomiasis, fever).
-cause_of(schistosomiasis, possible_exposure_to_animal_fluids).
+symptom_of(active_TB, cough_with_blood).
+symptom_of(active_TB, cough_with_phlegm).
+symptom_of(active_TB, chest_pain).
+symptom_of(active_TB, breathing_pain).
+symptom_of(active_TB, coughing_pain).
+symptom_of(active_TB, fever).
+symptom_of(active_TB, chills).
+symptom_of(active_TB, night_sweats).
+symptom_of(active_TB, weight_loss).
+symptom_of(active_TB, lack_of_appetite).
+symptom_of(active_TB, fatigue).
+symptom_of(active_TB, weakness).
+cause_of(active_TB, exposure_to_people_with_TB).
+cause_of(active_TB, travel_to_tropical_regions).
+cause_of(active_TB, living_or_working_in_tight_places).
 
 symptom_of(leptospirosis, fever).
 symptom_of(leptospirosis, headache).
@@ -57,6 +60,40 @@ symptom_of(leptospirosis, rash).
 cause_of(leptospirosis, possible_exposure_to_animal_fluids).
 cause_of(leptospirosis, contaminated_food_or_beverages).
 
+symptom_of(dengue, nausea).
+symptom_of(dengue, vomiting).
+symptom_of(dengue, eye_pain).
+symptom_of(dengue, muscle_pain).
+symptom_of(dengue, joint_pain).
+symptom_of(dengue, abdominal_pain).
+symptom_of(dengue, blood_in_urine_and/or_feces).
+symptom_of(dengue, fatigue).
+symptom_of(dengue, weakness).
+cause_of(dengue, travel_to_tropical_regions).
+
+symptom_of(influenza, fever).
+symptom_of(influenza, cough).
+symptom_of(influenza, sore_throat).
+symptom_of(influenza, runny_nose).
+symptom_of(influenza, body_aches).
+symptom_of(influenza, headache).
+symptom_of(influenza, fatigue).
+symptom_of(influenza, vomiting).
+symptom_of(influenza, diarrhea).
+cause_of(influenza, exposure_to_people_with_flu).
+
+symptom_of(pneumonia, breathing_pain).
+symptom_of(pneumonia, coughing_pain).
+symptom_of(pneumonia, cough_with_phlegm).
+symptom_of(pneumonia, fatigue).
+symptom_of(pneumonia, fever).
+symptom_of(pneumonia, chills).
+symptom_of(pneumonia, nausea).
+symptom_of(pneumonia, vomiting).
+symptom_of(pneumonia, diarrhea).
+symptom_of(pneumonia, difficulty_breathing).
+cause_of(pneumonia, has_hiv_or_aids).
+symptom_of(pneumonia, has_chronic_pulmonary_diseases).
 
 symptom_of(hepatitis_b, fever).
 symptom_of(hepatitis_b, fatigue).
@@ -70,6 +107,42 @@ symptom_of(hepatitis_b, joint_pain).
 symptom_of(hepatitis_b, jaundice).
 cause_of(hepatitis_b, contact_with_bodily_fluids).
 cause_of(hepatitis_b, sexually_active).
+
+symptom_of(malaria, fever).
+symptom_of(malaria, chills).
+symptom_of(malaria, headache).
+symptom_of(malaria, body_aches).
+symptom_of(malaria, fatigue).
+symptom_of(malaria, chest_pain).
+symptom_of(malaria, breathing_pain).
+symptom_of(malaria, coughing_pain).
+symptom_of(malaria, cough).
+symptom_of(malaria, diarrhea).
+symptom_of(malaria, nausea).
+symptom_of(malaria, vomiting).
+cause_of(malaria, travel_to_tropical_regions).
+
+symptom_of(schistosomiasis, rash).
+symptom_of(schistosomiasis, itchy_skin).
+symptom_of(schistosomiasis, muscle_pain).
+symptom_of(schistosomiasis, stomach_pain).
+symptom_of(schistosomiasis, blood_in_urine_and/or_feces).
+symptom_of(schistosomiasis, difficulty_urinating).
+symptom_of(schistosomiasis, enlarged_liver).
+symptom_of(schistosomiasis, cough).
+symptom_of(schistosomiasis, fever).
+cause_of(schistosomiasis, possible_exposure_to_animal_fluids).
+
+symptom_of(hookworm, itchy_skin).
+symptom_of(hookworm, rash).
+symptom_of(hookworm, abdominal_pain).
+symptom_of(hookworm, diarrhea).
+symptom_of(hookworm, lack_of_appetite).
+symptom_of(hookworm, weight_loss).
+symptom_of(hookworm, fatigue).
+symptom_of(hookworm, anemia).
+cause_of(hookworm, recently_outdoors_with_poor_sanitation_and_hot_temperatures).
+cause_of(hookworm, recently_contacted_with_soil)
 
 :- dynamic checked_symptoms/1.
 :- dynamic symptom_flag/1.
