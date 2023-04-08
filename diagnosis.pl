9e6b50a6d36b97f07f3a0db5b848f97072c8d56f
Ysobella Torio <94037762+ysobella@users.noreply.github.com>
2023-04-08 14:17:19 +0800

    Update diagnosis.pl

--- diagnosis.pl
+++ diagnosis.pl
@@ -6,144 +6,144 @@ weight(3, _, 0.8).
 weight(4, _, 1).
 
 % Define the symptoms and causes for each disease
-symptom_of(typhoid_fever, fever).
-symptom_of(typhoid_fever, headache).
-symptom_of(typhoid_fever, fatigue).
-symptom_of(typhoid_fever, cough).
-symptom_of(typhoid_fever, constipation).
-symptom_of(typhoid_fever, body_aches).
+symptom_of('typhoid fever', 'fever').
+symptom_of('typhoid fever', 'headache').
+symptom_of('typhoid fever', 'fatigue').
+symptom_of('typhoid fever', 'cough').
+symptom_of('typhoid fever', 'constipation').
+symptom_of('typhoid fever', 'body aches').
 
-symptom_of(hepatitis_a, jaundice).
-symptom_of(hepatitis_a, lack_of_appetite).
-symptom_of(hepatitis_a, upset_stomach).
-symptom_of(hepatitis_a, vomiting).
-symptom_of(hepatitis_a, stomach_pain).
-symptom_of(hepatitis_a, fever).
-symptom_of(hepatitis_a, dark_urine).
-symptom_of(hepatitis_a, light_colored_stools).
-symptom_of(hepatitis_a, diarrhea).
-symptom_of(hepatitis_a, joint_pain).
-symptom_of(hepatitis_a, fatigue).
+symptom_of('hepatitis a', 'jaundice').
+symptom_of('hepatitis a', 'lack of appetite').
+symptom_of('hepatitis a', 'upset stomach').
+symptom_of('hepatitis a', 'vomiting').
+symptom_of('hepatitis a', 'stomach pain').
+symptom_of('hepatitis a', 'fever').
+symptom_of('hepatitis a', 'dark urine').
+symptom_of('hepatitis a', 'light colored stools').
+symptom_of('hepatitis a', 'diarrhea').
+symptom_of('hepatitis a', 'joint pain').
+symptom_of('hepatitis a', 'fatigue').
 
-symptom_of(active_TB, cough_with_blood).
-symptom_of(active_TB, cough_with_phlegm).
-symptom_of(active_TB, chest_pain).
-symptom_of(active_TB, breathing_pain).
-symptom_of(active_TB, coughing_pain).
-symptom_of(active_TB, fever).
-symptom_of(active_TB, chills).
-symptom_of(active_TB, night_sweats).
-symptom_of(active_TB, weight_loss).
-symptom_of(active_TB, lack_of_appetite).
-symptom_of(active_TB, fatigue).
-symptom_of(active_TB, weakness).
+symptom_of('active TB', 'cough with blood').
+symptom_of('active TB', 'cough with phlegm').
+symptom_of('active TB', 'chest pain').
+symptom_of('active TB', 'breathing pain').
+symptom_of('active TB', 'coughing pain').
+symptom_of('active TB', 'fever').
+symptom_of('active TB', 'chills').
+symptom_of('active TB', 'night sweats').
+symptom_of('active TB', 'weight loss').
+symptom_of('active TB', 'lack of appetite').
+symptom_of('active TB', 'fatigue').
+symptom_of('active TB', 'weakness').
 
-symptom_of(leptospirosis, fever).
-symptom_of(leptospirosis, headache).
-symptom_of(leptospirosis, chills).
-symptom_of(leptospirosis, body_aches).
-symptom_of(leptospirosis, vomiting).
-symptom_of(leptospirosis, jaundice).
-symptom_of(leptospirosis, red_eyes).
-symptom_of(leptospirosis, abdominal_pain).
-symptom_of(leptospirosis, diarrhea).
-symptom_of(leptospirosis, rash).
+symptom_of(leptospirosis, 'fever').
+symptom_of(leptospirosis, 'headache').
+symptom_of(leptospirosis, 'chills').
+symptom_of(leptospirosis, 'body aches').
+symptom_of(leptospirosis, 'vomiting').
+symptom_of(leptospirosis, 'jaundice').
+symptom_of(leptospirosis, 'red eyes').
+symptom_of(leptospirosis, 'abdominal pain').
+symptom_of(leptospirosis, 'diarrhea').
+symptom_of(leptospirosis, 'rash').
 
-symptom_of(dengue, nausea).
-symptom_of(dengue, vomiting).
-symptom_of(dengue, eye_pain).
-symptom_of(dengue, muscle_pain).
-symptom_of(dengue, joint_pain).
-symptom_of(dengue, abdominal_pain).
-symptom_of(dengue, blood_in_urine_and/or_feces).
-symptom_of(dengue, fatigue).
-symptom_of(dengue, weakness).
+symptom_of(dengue, 'nausea').
+symptom_of(dengue, 'vomiting').
+symptom_of(dengue, 'eye pain').
+symptom_of(dengue, 'muscle pain').
+symptom_of(dengue, 'joint pain').
+symptom_of(dengue, 'abdominal pain').
+symptom_of(dengue, 'blood in urine and/or feces').
+symptom_of(dengue, 'fatigue').
+symptom_of(dengue, 'weakness').
 
-symptom_of(influenza, fever).
-symptom_of(influenza, cough).
-symptom_of(influenza, sore_throat).
-symptom_of(influenza, runny_nose).
-symptom_of(influenza, body_aches).
-symptom_of(influenza, headache).
-symptom_of(influenza, fatigue).
-symptom_of(influenza, vomiting).
-symptom_of(influenza, diarrhea).
+symptom_of(influenza, 'fever').
+symptom_of(influenza, 'cough').
+symptom_of(influenza, 'sore throat').
+symptom_of(influenza, 'runny nose').
+symptom_of(influenza, 'body aches').
+symptom_of(influenza, 'headache').
+symptom_of(influenza, 'fatigue').
+symptom_of(influenza, 'vomiting').
+symptom_of(influenza, 'diarrhea').
 
-symptom_of(pneumonia, breathing_pain).
-symptom_of(pneumonia, coughing_pain).
-symptom_of(pneumonia, cough_with_phlegm).
-symptom_of(pneumonia, fatigue).
-symptom_of(pneumonia, fever).
-symptom_of(pneumonia, chills).
-symptom_of(pneumonia, nausea).
-symptom_of(pneumonia, vomiting).
-symptom_of(pneumonia, diarrhea).
-symptom_of(pneumonia, difficulty_breathing).
+symptom_of(pneumonia, 'breathing pain').
+symptom_of(pneumonia, 'coughing pain').
+symptom_of(pneumonia, 'cough with phlegm').
+symptom_of(pneumonia, 'fatigue').
+symptom_of(pneumonia, 'fever').
+symptom_of(pneumonia, 'chills').
+symptom_of(pneumonia, 'nausea').
+symptom_of(pneumonia, 'vomiting').
+symptom_of(pneumonia, 'diarrhea').
+symptom_of(pneumonia, 'difficulty breathing').
 
-symptom_of(hepatitis_b, fever).
-symptom_of(hepatitis_b, fatigue).
-symptom_of(hepatitis_b, lack_of_appetite).
-symptom_of(hepatitis_b, nausea).
-symptom_of(hepatitis_b, vomiting).
-symptom_of(hepatitis_b, abdominal_pain).
-symptom_of(hepatitis_b, dark_urine).
-symptom_of(hepatitis_b, clay_colored_stool).
-symptom_of(hepatitis_b, joint_pain).
-symptom_of(hepatitis_b, jaundice).
+symptom_of('hepatitis b', 'fever').
+symptom_of('hepatitis b', 'fatigue').
+symptom_of('hepatitis b', 'lack of appetite').
+symptom_of('hepatitis b', 'nausea').
+symptom_of('hepatitis b', 'vomiting').
+symptom_of('hepatitis b', 'abdominal pain').
+symptom_of('hepatitis b', 'dark urine').
+symptom_of('hepatitis b', 'clay colored stool').
+symptom_of('hepatitis b', 'joint pain').
+symptom_of('hepatitis b', 'jaundice').
 
-symptom_of(malaria, fever).
-symptom_of(malaria, chills).
-symptom_of(malaria, headache).
-symptom_of(malaria, body_aches).
-symptom_of(malaria, fatigue).
-symptom_of(malaria, chest_pain).
-symptom_of(malaria, breathing_pain).
-symptom_of(malaria, coughing_pain).
-symptom_of(malaria, cough).
-symptom_of(malaria, diarrhea).
-symptom_of(malaria, nausea).
-symptom_of(malaria, vomiting).
+symptom_of(malaria, 'fever').
+symptom_of(malaria, 'chills').
+symptom_of(malaria, 'headache').
+symptom_of(malaria, 'body aches').
+symptom_of(malaria, 'fatigue').
+symptom_of(malaria, 'chest pain').
+symptom_of(malaria, 'breathing pain').
+symptom_of(malaria, 'coughing pain').
+symptom_of(malaria, 'cough').
+symptom_of(malaria, 'diarrhea').
+symptom_of(malaria, 'nausea').
+symptom_of(malaria, 'vomiting').
 
-symptom_of(schistosomiasis, rash).
-symptom_of(schistosomiasis, itchy_skin).
-symptom_of(schistosomiasis, muscle_pain).
-symptom_of(schistosomiasis, stomach_pain).
-symptom_of(schistosomiasis, blood_in_urine_and/or_feces).
-symptom_of(schistosomiasis, difficulty_urinating).
-symptom_of(schistosomiasis, enlarged_liver).
-symptom_of(schistosomiasis, cough).
-symptom_of(schistosomiasis, fever).
+symptom_of(schistosomiasis, 'rash').
+symptom_of(schistosomiasis, 'itchy skin').
+symptom_of(schistosomiasis, 'muscle pain').
+symptom_of(schistosomiasis, 'stomach pain').
+symptom_of(schistosomiasis, 'blood in urine and/or feces').
+symptom_of(schistosomiasis, 'difficulty urinating').
+symptom_of(schistosomiasis, 'enlarged liver').
+symptom_of(schistosomiasis, 'cough').
+symptom_of(schistosomiasis, 'fever').
 
-symptom_of(hookworm, itchy_skin).
-symptom_of(hookworm, rash).
-symptom_of(hookworm, abdominal_pain).
-symptom_of(hookworm, diarrhea).
-symptom_of(hookworm, lack_of_appetite).
-symptom_of(hookworm, weight_loss).
-symptom_of(hookworm, fatigue).
-symptom_of(hookworm, anemia).
+symptom_of(hookworm, 'itchy skin').
+symptom_of(hookworm, 'rash').
+symptom_of(hookworm, 'abdominal pain').
+symptom_of(hookworm, 'diarrhea').
+symptom_of(hookworm, 'lack of appetite').
+symptom_of(hookworm, 'weight loss').
+symptom_of(hookworm, 'fatigue').
+symptom_of(hookworm, 'anemia').
 
-cause_of(typhoid_fever, contaminated_food_or_beverages).
-cause_of(typhoid_fever, travel_to_tropical_regions).
-cause_of(hepatitis_a, poor_sanitation).
-cause_of(hepatitis_a, sexually_active).
-cause_of(hepatitis_a, contact_with_bodily_fluids).
-cause_of(hepatitis_a, contaminated_food_or_beverages).
-cause_of(active_TB, exposure_to_people_with_TB).
-cause_of(active_TB, travel_to_tropical_regions).
-cause_of(active_TB, living_or_working_in_tight_places).
-cause_of(leptospirosis, possible_exposure_to_animal_fluids).
-cause_of(leptospirosis, contaminated_food_or_beverages).
-cause_of(dengue, travel_to_tropical_regions).
-cause_of(influenza, exposure_to_people_with_flu).
-cause_of(pneumonia, has_hiv_or_aids).
-cause_of(pneumonia, has_chronic_pulmonary_diseases).
-cause_of(hepatitis_b, contact_with_bodily_fluids).
-cause_of(hepatitis_b, sexually_active).
-cause_of(malaria, travel_to_tropical_regions).
-cause_of(schistosomiasis, possible_exposure_to_animal_fluids).
-cause_of(hookworm, recently_outdoors_with_poor_sanitation_and_hot_temperatures).
-cause_of(hookworm, recently_contacted_with_soil).
+cause_of('typhoid fever', 'intake of contaminated food or beverages').
+cause_of('typhoid fever', 'travel to tropical regions').
+cause_of('hepatitis a', 'poor sanitation').
+cause_of('hepatitis a', 'sexually active').
+cause_of('hepatitis a', 'contact with bodily fluids').
+cause_of('hepatitis a', 'intake of contaminated food or beverages').
+cause_of('active TB', 'exposure to people with TB').
+cause_of('active TB', 'travel to tropical regions').
+cause_of('active TB', 'living or working in tight places').
+cause_of(leptospirosis, 'exposure to animal fluids').
+cause_of(leptospirosis, 'intake of contaminated food or beverages').
+cause_of(dengue, 'travel to tropical regions').
+cause_of(influenza, 'exposure to people with flu').
+cause_of(pneumonia, 'has HIV or AIDS').
+cause_of(pneumonia, 'has chronic pulmonary diseases').
+cause_of('hepatitis b', 'contact with bodily fluids').
+cause_of('hepatitis b', 'sexually active').
+cause_of(malaria, 'travel to tropical regions').
+cause_of(schistosomiasis, 'exposure to animal fluids').
+cause_of(hookworm, 'stayed outdoors with poor sanitation and hot temperatures').
+cause_of(hookworm, 'bare contact with soil').
 
 :- dynamic checked_symptoms/1.
 :- dynamic symptom_flag/1.
@@ -237,7 +237,9 @@ disease_risk([Disease | RestDiseases], [Risk | RestRisks]) :-
 	assertz(risk_ratings(Risk)),nl,
     disease_risk(RestDiseases, RestRisks).
 
-% Generates a list of potential diseases given the patients HPI and Chief Complaint
+% Generates a list of potential diseases given the patients HPI and
+% Chief Complaint
+
 identify_potential_disease(Diseases, Complaint, Severity, Causes) :-
     weight(Severity, _, Score),
     add_symptom(_, Complaint, Score),
@@ -256,8 +258,8 @@ identify_potential_disease(Diseases, Complaint, Severity, Causes) :-
     ( Placeholder = []
     -> findall(Disease, (symptom_of(Disease, Complaint) ; (cause_of(Disease, Cause), member(Cause, Causes)) ),DiseasesWithNoRepeats), %Case 3
         list_to_set(DiseasesWithNoRepeats, Diseases), nl
-    ;    
-        list_to_set(Placeholder, Diseases), nl,
+    ;
+        list_to_set(Placeholder, Diseases), nl
         ),
     write('TO REMOVE: You may have the following diseases: '), write(Diseases), nl, nl.
 
