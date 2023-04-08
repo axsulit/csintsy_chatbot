ff47c8ae579e52de129445e68197e0f2499d1470
Ysobella Torio <94037762+ysobella@users.noreply.github.com>
2023-04-08 14:18:01 +0800

    Update hpi.pl

--- hpi.pl
+++ hpi.pl
@@ -1,36 +1,36 @@
 % Rules for identifying the history of present illness (HPI)
 
 % Define the causes
-cause(poor_sanitation).
-cause(travel_to_tropical_regions).
-cause(contact_with_bodily_fluids).
-cause(possible_exposure_to_animal_fluids).
-cause(sexually_active).
-cause(contaminated_food_or_beverages).
-cause(has_chronic_liver_diseases).
-cause(has_chronic_pulmonary_diseases).
-cause(has_hiv_or_aids).
-cause(exposure_to_people_with_TB).
-cause(exposure_to_people_with_flu).
-cause(living_or_working_in_tight_places).
-cause(recently_outdoors_with_poor_sanitation_and_hot_temperatures).
-cause(recently_contacted_with_soil).
+cause('poor sanitation').
+cause('travel to tropical regions').
+cause('contact with bodily fluids').
+cause('exposure to animal fluids').
+cause('sexually active').
+cause('intake of contaminated food or beverages').
+cause('has chronic liver diseases').
+cause('has chronic pulmonary diseases').
+cause('has HIV or AIDS').
+cause('exposure to people with TB').
+cause('exposure to people with flu').
+cause('living or working in tight places').
+cause('stayed outdoors with poor sanitation and hot temperatures').
+cause('bare contact with soil').
 
 % Define the possible answers for each cause
-question(poor_sanitation, 'Do you have any issues or difficulties maintaining proper hygiene and sanitation practices?').
-question(contaminated_food_or_beverages, 'Have you ingested any food or beverages that may have been contaminated within the past two weeks?').
-question(living_or_working_in_tight_places, 'Have you been residing or employed in cramped or confined spaces?').
-question(sexually_active, 'Have you engaged in unprotected sexual activity within the past 6 months?').
-question(contact_with_bodily_fluids, 'Did you have contact with bodily fluids within the last two weeks?').
-question(has_hiv_or_aids, 'Do you have HIV or AIDS?').
-question(has_chronic_liver_diseases, 'Do you have a history of chronic liver diseases?').
-question(has_chronic_pulmonary_diseases, 'Do you have a history of chronic pulmonary disease?').
-question(exposure_to_people_with_TB, 'Have you been in contact with anyone who has tuberculosis within the past two weeks?').
-question(exposure_to_people_with_flu, 'Have you come into contact with anyone who has had the flu within the past two weeks?').
-question(travel_to_tropical_regions, 'Have you visited any tropical regions within the past two weeks?').
-question(recently_outdoors_with_poor_sanitation_and_hot_temperatures, 'Have you spent time outdoors in areas with inadequate sanitation and hot temperatures within the past two weeks?').
-question(possible_exposure_to_animal_fluids, 'Have you come into contact with animal fluids within the past two weeks?').
-question(recently_contacted_with_soil, 'Have you had any bare contact with soil within the past two weeks?').
+question('poor sanitation', 'Do you have any issues or difficulties maintaining proper hygiene and sanitation practices?').
+question('intake of contaminated food or beverages', 'Have you ingested any food or beverages that may have been contaminated within the past two weeks?').
+question('living or working in tight places', 'Have you been residing or employed in cramped or confined spaces?').
+question('sexually active', 'Have you engaged in unprotected sexual activity within the past 6 months?').
+question('contact with bodily fluids', 'Did you have contact with bodily fluids within the last two weeks?').
+question('has HIV or AIDS', 'Do you have HIV or AIDS?').
+question('has chronic liver diseases', 'Do you have a history of chronic liver diseases?').
+question('has chronic pulmonary diseases', 'Do you have a history of chronic pulmonary disease?').
+question('exposure to people with TB', 'Have you been in contact with anyone who has tuberculosis within the past two weeks?').
+question('exposure to people with flu', 'Have you come into contact with anyone who has had the flu within the past two weeks?').
+question('travel to tropical regions', 'Have you visited any tropical regions within the past two weeks?').
+question('stayed outdoors with poor sanitation and hot temperatures', 'Have you spent time outdoors in areas with inadequate sanitation and hot temperatures within the past two weeks?').
+question('exposure to animal fluids', 'Have you come into contact with animal fluids within the past two weeks?').
+question('bare contact with soil', 'Have you had any bare contact with soil within the past two weeks?').
 
 
 ask_question(Patient, Cause, Prompt) :-
@@ -38,7 +38,7 @@ ask_question(Patient, Cause, Prompt) :-
     write('Answer: '), read(Answer), nl,
     (member(Answer, [yes, no])
     -> (Answer == yes, assertz(symptom(Patient, Cause)))
-       
+
     ;   write('Invalid input. Please enter either "yes" or "no".'), nl,
         ask_question(Patient, Cause, Prompt)
     ).
